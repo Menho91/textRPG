f7b88771713a9ec7fb03791f2585f2b2d9fa5245
Menho91 <agh914@naver.com>
2023-11-29 16:50:19 +0900

    Player::HealHP, HealMP 함수 기능 추가

--- textRPG/Character.cpp
+++ textRPG/Character.cpp
@@ -13,8 +13,24 @@ void Player::HealHP(int hp)
 {
 	if (health + hp > maxHP)
 	{
-
+		cout << "체력이 " << maxHP - health << " 회복되었습니다. 현재 체력 : " << health << endl;
+		health = maxHP;
+		return;
 	}
+	health += hp;
+	cout << "체력이 " << hp << " 회복되었습니다. 현재 체력 : " << health << endl;
+}
+
+void Player::HealMP(int mp)
+{
+	if (magic + mp > maxMP)
+	{
+		cout << "마나가 " << maxMP - magic << " 회복되었습니다. 현재 마나 : " << health << endl;
+		magic = maxMP;
+		return;
+	}
+	magic += mp;
+	cout << "마나가 " << mp << " 회복되었습니다. 현재 마나 : " << magic << endl;
 }
 
 
